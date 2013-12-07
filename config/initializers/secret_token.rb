bd64fe7bf2a9aba411fcd7a62462a69fa8cc5646
John Doe <adam.q.pruett@gmail.com>
2013-12-07 09:41:01 -0800

    HW_11.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HW111::Application.config.secret_key_base = '2b0394f1c8a27677a27e8021a6ff2fc72673be415b3c1c7ce741d236ad90460b54316130de47e43c725978bdd85ac97e52574194a0d92a146231ff05adf56db1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HW111::Application.config.session_store :cookie_store, key: '_HW_11_1_session'
